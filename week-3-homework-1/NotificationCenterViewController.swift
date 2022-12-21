3376c18d5a782e2738d6be29a8c69a1c05f63f09
kamilcal <kamilcaal@gmail.com>
2022-12-21 16:05:21 +0300

    storyboard structures created

---
+++ week-3-homework-1/NotificationCenterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationCenterViewController.swift
+//  week-3-homework-1
+//
+//  Created by kamilcal on 21.12.2022.
+//
+
+import UIKit
+
+class NotificationCenterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ week-3-homework-1/ProtocolViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProtocolViewController.swift
+//  week-3-homework-1
+//
+//  Created by kamilcal on 21.12.2022.
+//
+
+import UIKit
+
+class ProtocolViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
